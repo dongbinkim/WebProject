0283ff0650968493bb40c155837ce4bc632d1876
dongbin <ehdqls357@naver.com>
2015-11-16 00:38:14 +0900

    install package

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var session = require('express-session');
+var methodOverride = require('method-override');
+var flash = require('connect-flash');
+var mongoose   = require('mongoose');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -13,13 +17,20 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+if (app.get('env') === 'development') {
+  app.locals.pretty = true;
+}
 
+// mongodb connect
+mongoose.connect('mongodb://dongbin:dlem13587946@ds045064.mongolab.com:45064/wp');
+mongoose.connection.on('error', console.log);
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(methodOverride('_method', {methods: ['POST', 'GET']}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
