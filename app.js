e8564599c65cbb9758bee262530fb85d25099774
dongbin <ehdqls357@naver.com>
2015-11-21 00:33:52 +0900

    user page

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var flash = require('connect-flash');
 var mongoose   = require('mongoose');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
+var user = require('./routes/user');
 
 var app = express();
 
@@ -49,7 +49,7 @@ app.use(function(req, res, next) {
 });
 
 app.use('/', routes);
-app.use('/users', users);
+app.use('/user', user);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -2,32 +2,60 @@ var express = require('express');
     User = require('../models/User');
 var router = express.Router();
 
-// homepage
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'homepage' });
-});
-// login page
-router.get('/users', function(req, res, next) {
-  res.render('users',{title: 'users'});
-});
-  // 값 입력
-router.post('', function(req, res, next){
 
-})
+// home page
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'home' });
+});
+
+// user page
+router.get('/user', function(req, res, next) {
+  res.render('user/index',{title: 'user'});
+});
+
+
+// signin page - 로그인 페이지
+router.get('/signin', function(req, res, next) {
+  res.render('signin');
+});
+
+router.post('/signin', function(req, res, next) {
+  User.findOne({email: req.body.email}, function(err, user) {
+    if (err) {
+      res.render('error', {message: "Error", error: err});
+    } else if (!user) {
+      req.flash('danger', '존재하지 않는 사용자 입니다.');
+      res.redirect('back');
+    } else if (user.password !== req.body.password) {
+      req.flash('danger', '비밀번호가 일치하지 않습니다.');
+      res.redirect('back');
+    } else {
+      req.session.user = user;
+      req.flash('success', '로그인 되었습니다.');
+      res.redirect('/join');
+    }
+  });
+});
+
+
+// join page
+router.get('/join', function(req, res, next) {
+  res.render('join/index',{title: 'join'});
+});
 // forms page
-router.get('/forms', function(req, res, next) {
-  res.render('forms',{title: 'forms'});
+router.get('/form', function(req, res, next) {
+  res.render('form/index',{title: 'form'});
 });
-//quest page
-router.get('/quest', function(req, res, next) {
-  res.render('quest',{title: 'quest'});
+// preivew page
+router.get('/preview', function(req, res, next) {
+  res.render('preview/index',{title: 'preview'});
 });
-//answer page
-router.get('/ans', function(req, res, next) {
-  res.render('ans',{title: 'ans'});
+// collect page
+router.get('/collect', function(req, res, next) {
+  res.render('collect/index',{title: 'collect'});
 });
 // return homepage
-router.get('/ans', function(req, res, next) {
+router.get('/collect', function(req, res, next) {
   res.redirect('/');
 });
 
